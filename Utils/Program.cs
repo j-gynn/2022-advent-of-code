3d260e051e54ef15a3d6a2129197b2287e5b0e34
J Gynn <48025294+j-gynn@users.noreply.github.com>
2023-06-02 17:20:16 +0100

    Utils created

---
+++ Utils/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Utils 
+{
+    public class Input 
+    {
+        public static void Main(string[] args)
+        { 
+
+        }
+        public static string getInput() {
+            string? input;
+            bool ok = false;
+            do
+            {
+                Console.WriteLine("Provide a path for the file:");
+                input = Console.ReadLine();
+
+                if (input == null) {
+                    Console.WriteLine("No input received!");
+                    continue;
+                }
+
+                if (!File.Exists(input)) {
+                    Console.WriteLine("File does not exist.");
+                    continue;
+                }
+
+                // if we get here, the directory exists so we should be good to go.
+                ok = true;
+            } while (!ok);
+            return input;
+        }
+    }
+}
