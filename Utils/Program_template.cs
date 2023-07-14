528d0a31a740dedede3c174eb55433cf9e7e4d3b
J Gynn <48025294+j-gynn@users.noreply.github.com>
2023-07-14 13:26:39 +0100

    program template

---
+++ Utils/Program_template.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Utils;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input = 
+            Utils.Input.getInput();
+            
+
+            using (StreamReader sr = new StreamReader(input))
+            {
+                
+            }
+        }
+    }
+}
